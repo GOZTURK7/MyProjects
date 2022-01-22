c864710ae59614b62ab07b048c041a9fefa2a3f9
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2022-01-23 00:22:41 +0300

    January 22'th EOD

--- javafunctional/src/main/java/functionalinterface/_Function.java
+++ javafunctional/src/main/java/functionalinterface/_Function.java
@@ -1,5 +1,6 @@
 package functionalinterface;
 
+import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class _Function {
@@ -8,16 +9,27 @@ public class _Function {
     public static void main(String[] args) {
       int increment=  incrementByOne(1);
         System.out.println(increment);
-
+        // FUNCTİON takes 1 argument and produces 1 result
        int increment2= increamentByOneFunction.apply(1); // apply parametre alıyor.
         System.out.println(increment2);
 
         int multiply= multiplyBy10Function.apply(increment2); // multiply
         System.out.println(multiply);
 
+        // CHAINING FUNCTİONS --> ZİNCİRLEME FOKSİYON andThen ile yapıyoruz
         Function<Integer, Integer> addBy1AndThenMultiplyBy10 = increamentByOneFunction.andThen(multiplyBy10Function);
 
+        System.out.println(addBy1AndThenMultiplyBy10.apply(4));
+
+
+        // BİFUNCTİON  takes 2 argument and produces 1 result
+       System.out.println(incrementByOneAndMultiply(4,100));
+
+
+
     }
+
+    // FUNCTIONS
                     //parameter, ReturnType                      parameter
     static Function<Integer, Integer> increamentByOneFunction = number -> number + 1; // aşağıdaki ile tamamen aynı
 
@@ -26,4 +38,21 @@ public class _Function {
     static int incrementByOne(int number){
         return number +1;
     }
+
+
+
+
+    // BIFUNCTIONS
+
+            //<parameter, parameter, returntype>
+    static BiFunction<Integer, Integer, Integer> incrementByOneAndMultiplyBiFunction =
+        (numberToIncrementByOne, numbertoMultiplyby)->(numberToIncrementByOne +1) * numbertoMultiplyby;
+
+
+    static int incrementByOneAndMultiply(int number, int numToMultiply){
+        return (number +1) * numToMultiply;
+    }
+
+
+
 }
