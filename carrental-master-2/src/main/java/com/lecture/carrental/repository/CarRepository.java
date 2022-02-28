80882d13eb158e20ac0908365cd525a28432e19b
Gökhan Öztürk <gokhan.89.ozturk@gmail.com>
2022-03-01 01:09:59 +0300

    February 28'th EOD

--- carrental-master-2/src/main/java/com/lecture/carrental/repository/CarRepository.java
+++ carrental-master-2/src/main/java/com/lecture/carrental/repository/CarRepository.java
@@ -1,11 +1,24 @@
 package com.lecture.carrental.repository;
 
 import com.lecture.carrental.domain.Car;
+import com.lecture.carrental.dto.CarDTO;
+import com.lecture.carrental.exception.ResourceNotFoundException;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 @Transactional
 public interface CarRepository extends JpaRepository<Car, Long> {
+
+    @Query("SELECT new com.lecture.carrental.dto.CarDTO(c) FROM Car c")
+    public List<CarDTO> findAllCar();
+
+//    @Query("SELECT new com.lecture.carrental.dto.CarDTO(c) FROM Car c WHERE car.id = ?id") aşağıdakin abstract method bu query'yi yazıyor arka planda JPA
+    Optional<CarDTO> findByIdOrderById (Long id) throws ResourceNotFoundException;
+
 }
--- carrental-master-2/src/main/java/com/lecture/carrental/service/CarService.java
+++ carrental-master-2/src/main/java/com/lecture/carrental/service/CarService.java
@@ -3,6 +3,7 @@ package com.lecture.carrental.service;
 
 import com.lecture.carrental.domain.Car;
 import com.lecture.carrental.domain.FileDB;
+import com.lecture.carrental.dto.CarDTO;
 import com.lecture.carrental.exception.BadRequestException;
 import com.lecture.carrental.exception.ResourceNotFoundException;
 import com.lecture.carrental.repository.CarRepository;
@@ -11,6 +12,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @Service
@@ -20,6 +22,18 @@ public class CarService {
     private final CarRepository carRepository;
     private final FileDBRepository fileDBRepository;
     private final static String IMAGE_NOT_FOUND_MSG = "image with id %s not found";
+    private final static String CAR_NOT_FOUND_MSG = "car with id %d not found";
+
+
+
+    public List<CarDTO> fetchAllCars(){
+        return carRepository.findAllCar();
+    }
+
+    public CarDTO findById(Long id) throws ResourceNotFoundException {
+        return carRepository.findByIdOrderById(id).orElseThrow(
+                ()-> new ResourceNotFoundException(String.format(CAR_NOT_FOUND_MSG)));
+    }
 
     public void add(Car car, String imageId) throws BadRequestException {
         FileDB fileDB = fileDBRepository.findById(imageId).orElseThrow(
@@ -30,6 +44,32 @@ public class CarService {
         car.setImage(fileDBs);
 
         carRepository.save(car);
+    }
+
+    public void updateCar(Long id, String imageId, Car car) throws BadRequestException {
+        FileDB fileDB = fileDBRepository.findById(imageId).get();
+        Car carInfo = carRepository.getById(id);
+        if (carInfo.getBuiltIn()) {
+            throw new BadRequestException("You dont have permission to update car!");
+        }
+//        car.setBuiltIn(false);
+        Set<FileDB> fileDBS = new HashSet<>();
+        fileDBS.add(fileDB);
+        car.setImage(fileDBS);
+        car.setId(id);
+        carRepository.save(car);
+    }
+
+    public void removeById(Long id) throws BadRequestException {
+       Car car =  carRepository.findById(id).orElseThrow(()->
+               new ResourceNotFoundException(CAR_NOT_FOUND_MSG));
+
+       if(car.getBuiltIn()){
+           throw new BadRequestException("You dont have permisson to delete this car!");
+       }
+
+       carRepository.deleteById(id);
+
     }
 
 
